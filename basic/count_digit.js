5fbd139d35625c7c22bb52d753915587d01f9dcd
Bhushan Sharma <uibhushan@gmail.com>
2025-07-25 23:41:50 +0530

    added more  scenario

--- basic/count_digit.js
+++ basic/count_digit.js
@@ -3,6 +3,8 @@
 function countDigit(n) {
   if (n === 0) return 1;
   let count = 0;
+  // make posetive number always
+  n = Math.abs(n);
   while (n > 0) {
     n = Math.floor(n / 10);
     count++;
@@ -13,3 +15,4 @@ function countDigit(n) {
 console.log(countDigit(290));
 console.log(countDigit(0));
 console.log(countDigit(32343534353));
+console.log(countDigit(-98));
