f1fa7f0230956539e753ef610da946b09d9880bb
Bhushan Sharma <uibhushan@gmail.com>
2025-07-25 23:20:24 +0530

    Added more pattern

--- basic/start_pattern.js
+++ basic/start_pattern.js
@@ -29,7 +29,7 @@ function squreStart2(n) {
   }
 }
 
-console.log(squreStar2(4));
+console.log(squreStart2(4));
 
 /*
 Print below pattern: 
@@ -97,3 +97,63 @@ function tringlePattern3(n) {
 }
 
 console.log(tringlePattern3(5));
+
+/*
+1 
+1 0 
+1 0 1 
+1 0 1 0 
+1 0 1 0 1 
+*/
+
+function numberPatter(n) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+      if (j % 2 === 0) {
+        row += "1";
+      } else {
+        row += "0";
+      }
+    }
+    console.log(row);
+  }
+}
+
+console.log(numberPatter(5));
+
+function numberPatter1(n) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    let toggle = 0;
+    for (let j = 0; j < i + 1; j++) {
+      if (toggle === 0) {
+        toggle = 1;
+      } else {
+        toggle = 0;
+      }
+      row += toggle;
+    }
+    console.log(row);
+  }
+}
+
+console.log(numberPatter1(6));
+
+function numberPatter2(n) {
+  let toggle = 0;
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < i + 1; j++) {
+      if (toggle === 0) {
+        toggle = 1;
+      } else {
+        toggle = 0;
+      }
+      row += toggle;
+    }
+    console.log(row);
+  }
+}
+
+console.log(numberPatter2(5));
